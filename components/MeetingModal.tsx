4f78cdb36a3e8fc9dbcf188f5021ef18699ee368
omar <omarqais225@gmail.com>
2025-11-06 10:27:58 +0200

    fix input color

--- components/MeetingModal.tsx
+++ components/MeetingModal.tsx
@@ -24,7 +24,6 @@ const MeetingModal = ({ isOpen, onClose, title, className, children, handleClick
 ) => {
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
-
             <DialogContent className="flex w-full max-w-[520px]
                 flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
                 <DialogHeader>
